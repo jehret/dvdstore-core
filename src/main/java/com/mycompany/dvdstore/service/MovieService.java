7b4bb9fafa47a66f0792ce566265e5e518e0f7be
jp <jp.ehret@laposte.net>
2020-02-20 23:19:27 +1300

    Contrat

--- src/main/java/com/mycompany/dvdstore/service/MovieService.java
+++ src/main/java/com/mycompany/dvdstore/service/MovieService.java
@@ -2,10 +2,11 @@ package com.mycompany.dvdstore.service;
 
 import com.mycompany.dvdstore.entity.Movie;
 import com.mycompany.dvdstore.repository.GoLiveMovieRepository;
+import com.mycompany.dvdstore.repository.MovieRepositoryInterface;
 
 public class MovieService {
 
-    private GoLiveMovieRepository movieRepository;
+    private MovieRepositoryInterface movieRepository;
 
     public void registerMovie(Movie movie){
 
