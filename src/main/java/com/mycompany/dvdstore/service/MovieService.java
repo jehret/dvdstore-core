632927c4f19aedb6c81b513f7e0694694556f3de
jp <jp.ehret@laposte.net>
2020-02-20 23:31:34 +1300

    interface manquante

--- src/main/java/com/mycompany/dvdstore/service/MovieService.java
+++ src/main/java/com/mycompany/dvdstore/service/MovieService.java
@@ -3,9 +3,17 @@ package com.mycompany.dvdstore.service;
 import com.mycompany.dvdstore.entity.Movie;
 import com.mycompany.dvdstore.repository.MovieRepositoryInterface;
 
-public class MovieService {
+public class MovieService implements MovieServiceInterface{
 
     private MovieRepositoryInterface movieRepository;
+
+    public MovieRepositoryInterface getMovieRepository() {
+        return movieRepository;
+    }
+
+    public void setMovieRepository(MovieRepositoryInterface movieRepository) {
+        this.movieRepository = movieRepository;
+    }
 
     public void registerMovie(Movie movie){
 
