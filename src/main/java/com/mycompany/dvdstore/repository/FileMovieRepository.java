691c9cf75cc49e24e3f057fbc92b0d7ebf51a69f
jp <jp.ehret@laposte.net>
2020-03-25 15:23:06 +1300

    Property value

--- src/main/java/com/mycompany/dvdstore/repository/FileMovieRepository.java
+++ src/main/java/com/mycompany/dvdstore/repository/FileMovieRepository.java
@@ -2,15 +2,18 @@ package com.mycompany.dvdstore.repository;
 
 import com.mycompany.dvdstore.entity.Movie;
 
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
 public class FileMovieRepository implements MovieRepositoryInterface{
 
+    private File file;
+
     public void add(Movie movie){
         FileWriter writer;
         try{
-            writer=new FileWriter("C:\\temp\\movies.txt",true);
+            writer=new FileWriter(file,true);
             writer.write(movie.getTitle()+";"+movie.getGenre()+"\n");
             writer.close();
         }
@@ -20,4 +23,11 @@ public class FileMovieRepository implements MovieRepositoryInterface{
         System.out.println("The movie "+movie.getTitle()+" has been added.");
     }
 
+    public File getFile() {
+        return file;
+    }
+
+    public void setFile(File file) {
+        this.file = file;
+    }
 }
