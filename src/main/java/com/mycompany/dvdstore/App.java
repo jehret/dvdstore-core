3538d98259c5c1cf9c2ebae2cc9f4c1ad6ba28aa
jp <jp.ehret@laposte.net>
2020-04-09 11:42:57 +1200

    Java Config

--- src/main/java/com/mycompany/dvdstore/App.java
+++ src/main/java/com/mycompany/dvdstore/App.java
@@ -2,7 +2,7 @@ package com.mycompany.dvdstore;
 
 import com.mycompany.dvdstore.controller.MovieController;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 
 /**
@@ -13,7 +13,7 @@ public class App
 {
     public static void main( String[] args ) {
 
-        ApplicationContext context=new ClassPathXmlApplicationContext("applicationContext.xml");
+        ApplicationContext context=new AnnotationConfigApplicationContext(AppConfig.class);
 
         MovieController movieController=context.getBean(MovieController.class);
 
