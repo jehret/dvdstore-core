2c1abb05a7922d635b9ad5c258f7198a9bda838c
jp <jp.ehret@laposte.net>
2020-04-15 15:29:29 +1200

    Utiliser @SpringBootApplication et SpringApplication.run

--- src/main/java/com/mycompany/dvdstore/App.java
+++ src/main/java/com/mycompany/dvdstore/App.java
@@ -1,25 +1,22 @@
 package com.mycompany.dvdstore;
 
 import com.mycompany.dvdstore.controller.MovieController;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 
 
 /**
  *
  *
  */
-@Configuration
-@ComponentScan(basePackages = {"com.mycompany.dvdstore.repository.file","com.mycompany.dvdstore.service","com.mycompany.dvdstore.controller"})
-@PropertySource("classpath:application.properties")
+@SpringBootApplication
 public class App 
 {
     public static void main( String[] args ) {
 
-        ApplicationContext context=new AnnotationConfigApplicationContext(App.class);
+        //ApplicationContext context=new AnnotationConfigApplicationContext(App.class);
+        ApplicationContext context= SpringApplication.run(App.class);
 
         MovieController movieController=context.getBean(MovieController.class);
 
--- src/main/java/com/mycompany/dvdstore/repository/memory/MemoryMovieRepository.java
+++ src/main/java/com/mycompany/dvdstore/repository/memory/MemoryMovieRepository.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Repository;
 import java.util.ArrayList;
 import java.util.List;
 
-@Repository
+//@Repository
 public class MemoryMovieRepository implements MovieRepositoryInterface {
 
     private List<Movie> movies=new ArrayList<>();
