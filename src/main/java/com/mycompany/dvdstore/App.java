91ab6c8af19d92c2f56b6da88c649668b92cdf3e
jp <jp.ehret@laposte.net>
2020-04-15 20:20:48 +1200

    Spring Boot Parent

--- src/main/java/com/mycompany/dvdstore/App.java
+++ src/main/java/com/mycompany/dvdstore/App.java
@@ -16,7 +16,7 @@ public class App
     public static void main( String[] args ) {
 
         //ApplicationContext context=new AnnotationConfigApplicationContext(App.class);
-        ApplicationContext context= SpringApplication.run(App.class);
+        ApplicationContext context= SpringApplication.run(App.class,args);
 
         MovieController movieController=context.getBean(MovieController.class);
 
