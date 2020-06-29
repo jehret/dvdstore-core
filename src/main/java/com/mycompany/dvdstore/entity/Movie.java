db286ec23ed00f35aef4cd159d286c689bcfdb91
jp <jp.ehret@laposte.net>
2020-06-30 11:03:22 +1200

    spring data jpa

--- src/main/java/com/mycompany/dvdstore/entity/Movie.java
+++ src/main/java/com/mycompany/dvdstore/entity/Movie.java
@@ -1,10 +1,16 @@
 package com.mycompany.dvdstore.entity;
 
-import org.springframework.data.annotation.Id;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Movie {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
     private String genre;
