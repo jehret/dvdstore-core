6a3d630b19ad6393ff5aea9ba976efaa2b2f0829
jp <jp.ehret@laposte.net>
2020-07-01 18:37:53 +1200

    save

--- src/main/java/com/mycompany/dvdstore/service/DefaultMovieService.java
+++ src/main/java/com/mycompany/dvdstore/service/DefaultMovieService.java
@@ -4,6 +4,7 @@ import com.mycompany.dvdstore.entity.Movie;
 import com.mycompany.dvdstore.repository.MovieRepositoryInterface;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.NoSuchElementException;
@@ -23,6 +24,7 @@ public class DefaultMovieService implements MovieServiceInterface{
         this.movieRepository = movieRepository;
     }
 
+    @Transactional
     public Movie registerMovie(Movie movie){
         return movieRepository.save(movie);
     }
