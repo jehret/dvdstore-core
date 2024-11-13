e832791eadabc8143ef0892962eff4765b87b10f
Edouard Leroy <leroyedouard@protonmail.com>
2024-11-13 19:37:26 +0100

    TP1

--- src/test/java/com/mycompany/dvdstore/AppTest.java
+++ src/test/java/com/mycompany/dvdstore/AppTest.java
@@ -1,14 +1,13 @@
 package com.mycompany.dvdstore;
 
+import static org.junit.Assert.assertTrue;
 
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.Test;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
+public class AppTest
 {
     /**
      * Rigorous Test :-)
