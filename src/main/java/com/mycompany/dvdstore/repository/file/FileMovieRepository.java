0c4282fe8adc8a65686fd0a3f80ae6e145c33593
Jean-Philippe Ehret <jeanphiehret@gmail.com>
2024-12-31 21:55:31 +1300

    relational no longer jdbc as a dependency, throws Unsupported

--- src/main/java/com/mycompany/dvdstore/repository/file/FileMovieRepository.java
+++ src/main/java/com/mycompany/dvdstore/repository/file/FileMovieRepository.java
@@ -125,7 +125,7 @@ public class FileMovieRepository implements MovieRepositoryInterface {
      */
     @Override
     public void deleteAllById(Iterable<? extends Long> longs) {
-
+        throw new UnsupportedOperationException();
     }
 
 
